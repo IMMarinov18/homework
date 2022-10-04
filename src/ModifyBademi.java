d7d8fe012cfae05c68c19e1ac0ecdda214471159
IMMarinov18 <IMMarinov18@codingburgas.bg>
2022-10-04 15:40:19 +0300

    Initial commit

---
+++ src/ModifyBademi.java
@@ -0,0 +1,14 @@
+public class ModifyBademi {
+
+    public static void main(String[] args) {
+        Bademi BademiModify = new Bademi();
+
+        BademiModify.setName("Bademki");
+        BademiModify.setAge(3);
+
+        System.out.println(BademiModify.getName());
+        System.out.println(BademiModify.getAge());
+
+    }
+
+}
